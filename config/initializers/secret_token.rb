9390182fddd4f20d7d54396895b9e56957f8b561
NikolaBalog <offinger@gmail.com>
2012-04-27 22:41:27 +0200

    nikola

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipsta::Application.config.secret_token = 'aebfc2129605e88d27cb9ff8101d9960a25253d01097ddbd67c39dabbe7e1b6b2c5de0e337fe0bddd0458a7217b5ba9c706598cdae2e030c088b5a93c4f77122'
